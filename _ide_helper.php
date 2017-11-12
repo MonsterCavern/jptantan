4c7b3065a14b61cfeb07e839d49f25dc09821de8
Cosmos <cosmos@CsMBP.local>
2017-11-12 14:48:56 +0800

    save

--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.4.36 on 2017-11-06.
+ * Generated for Laravel 5.4.36 on 2017-11-10.
  *
  * @author Barry vd. Heuvel <barryvdh@gmail.com>
  * @see https://github.com/barryvdh/laravel-ide-helper
