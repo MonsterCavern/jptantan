42b0a30e2db8dfbd352de2f50aa9ae61d03be728
Cosmos <cosmos@Cosmos-iMac.local>
2018-05-31 17:39:08 +0800

    🔨 Refactor: Save

--- resources/vue/Vuetify/pages/Bokete.vue
+++ resources/vue/Vuetify/pages/Bokete.vue
@@ -1,55 +1,40 @@
 <template lang="html">
     <v-container>
-      Hello World 
-      <div v-for="vaule in bokete">
-        {{ vaule }}
-      </div>
+        <h1>Bokete</h1>
+
+        <v-list two-line>
+            <template v-for="(item, index) in bokete">
+                <v-list-tile :key="item.title" avatar ripple @click="toggle(index)">
+                    <v-list-tile-content v-html="item.source">
+                        <v-list-tile-sub-title class="text--primary">{{ item.headline }}</v-list-tile-sub-title>
+                    </v-list-tile-content>
+                    <v-list-tile-action>
+                        <v-list-tile-action-text>{{ item.created_at }}</v-list-tile-action-text>
+                        <v-icon v-if="selected.indexOf(index) < 0" color="grey lighten-1">star_border</v-icon>
+                        <v-icon v-else color="yellow darken-2">star</v-icon>
+                    </v-list-tile-action>
+                </v-list-tile>
+                <v-divider v-if="index + 1 < items.length" :key="index"></v-divider>
+            </template>
+        </v-list>
     </v-container>
 </template>
 
 <script>
 export default {
-    // Options / Data
     data() {
-        return {}
+        return {
+            selected: []
+        }
     },
-    // props: [],
-    // propsData: {},
     computed: {
         bokete() {
             return this.$store.state.bokete.data
         }
     },
-    // methods: {},
-    // watch: {},
-    // Options / DOM
-    // el () {},
-    // template: '',
-    // render () {},
-    // Options / Lifecycle Hooks
-    // beforeCreate () {},
-    // created () {},
-    // beforeMount () {},
     mounted() {
         this.$store.dispatch('bokete/init')
     }
-    // beforeUpdate () {},
-    // updated () {},
-    // activated () {},
-    // deactivated () {},
-    // beforeDestroy () {},
-    // destroyed () {},
-    // Options / Assets
-    // directives: {},
-    // filters: {},
-    // components: {},
-    // Options / Misc
-    // parent: null,
-    // mixins: [],
-    // name: '',
-    // extends: {},
-    // delimiters: [ '{{', '}}' ],
-    // functional: false
 }
 </script>
 
