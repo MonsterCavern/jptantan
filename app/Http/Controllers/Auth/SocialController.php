8e4320321bb0249d4b5ae74d20e2367f6daee8a7
Cosmo <cosmos-s@hotmail.com.tw>
2020-12-03 15:36:44 +0800

    ✨ Feat: 新增 GitHub Social Login 邏輯

--- app/Http/Controllers/Auth/SocialController.php
+++ app/Http/Controllers/Auth/SocialController.php
@@ -2,11 +2,20 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\User;
+use App\Services\UserService;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
 
 class SocialController extends Controller
 {
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+
     /**
      * 將用戶重定向到GitHub身份驗證頁面。
      *
@@ -24,8 +33,47 @@ class SocialController extends Controller
      */
     public function handleGitHubCallback()
     {
-        $user = Socialite::driver('github')->user();
-        dd($user);
-        // $user->token;
+        //
+        $response = Socialite::driver('github')->user();
+        $token    = $response->token;
+        $email    = $response->email;
+        $name     = $response->name;
+        $name     = $name ? $name : $response->nickname;
+        $name     = $name ? $name : strstr($email, '@', true);
+        //
+
+        $user = User::where('email', $response['email'])->first();
+
+        DB::beginTransaction();
+        try {
+            // 不存在則新增
+            if (! $user) {
+                $user = $this->userService->createUserFromSocialLogin([
+                    'name'   => $name,
+                    'email'  => $email,
+                    'status' => '未確認個人資料'
+                ]);
+            }
+            // 新增 token 紀錄
+            $token = $this->userService->createToken($user, 'github', $token);
+            //
+        } catch (\Throwable $th) {
+            DB::rollback();
+            throw $th;
+        }
+        DB::commit();
+
+        // 登入
+        Auth::guard()->login($user);
+
+        if ($user->status === '未確認個人資料') {
+            return redirect('/profile')->with('token', $token);
+        }
+
+        // if ($user->status === '未驗證信箱') {
+        //     return redirect('/profile')->with('token', $token);
+        // }
+
+        return redirect('/')->with('token', $token);
     }
 }
---
+++ app/Services/UserService.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Services;
+
+use App\User;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+
+class UserService
+{
+    public function createToken(User $user, $type, $token)
+    {
+        $token = $user->tokens()->create([
+          'type'  => $type,
+          'token' => $token
+        ]);
+
+        return $token;
+    }
+
+    public function createUserFromSocialLogin($attributes = [])
+    {
+        // 驗證必要參數
+        $user = User::create([
+          'name'     => $attributes['name'],
+          'email'    => $attributes['email'],
+          'password' => Hash::make(Str::random(10))
+        ]);
+
+        return $user;
+    }
+}
