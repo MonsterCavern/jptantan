4c33cc23af494ab83d1f71de990553af6b7cd2ad
d <d>
2021-01-02 17:41:49 +0800

    修正 未知路由

--- routes/web.php
+++ routes/web.php
@@ -12,14 +12,12 @@
 */
 
 Auth::routes();
-
-//
 Route::view('/', 'pure.index');
-// 未知路由導回首頁
-// Route::view('/{all}', 'pure.index')->where(['all' => '.*']);
-
 
 Route::get('messages', 'MessageController@viewDidLoad');
 Route::get('messages/{id}', 'MessageController@show');
 
 Route::post('messages', 'MessageController@onPost');
+
+// 未知路由導回首頁
+Route::view('/{all}', 'pure.index')->where(['all' => '.*']);
